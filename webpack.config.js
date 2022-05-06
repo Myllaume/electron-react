d08a170522f5f8e46beeac6648f82f4f4c030c23
Myllaume <guillaume.brioudes@myllaume.fr>
2022-05-06 20:54:13 +0200

    add webpack html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname),
         filename: 'bundle.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Naute',
+            template: './src/template.html'
+        })
+    ],
     module: {
         rules: [
             {
